de574494c3a946a08a4153f71de7e811e9bba03b
waityousea <1379613157@qq.com>
2021-02-07 17:20:55 +0800

    简单配置文件vue.config.js,整体引入ant-design-vue组件库

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     <h1>登录</h1>
+    <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
+    <a-config-provider :auto-insert-space-in-button="false">
+      <a-button type="primary">按钮</a-button>
+    </a-config-provider>
+    <a-button type="primary">按钮</a-button>
+    <a-button type="link">Link</a-button>
   </div>
 </template>
